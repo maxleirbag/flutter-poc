9ffbb83a9b0194a29e7488b30180f851dcf1765c
Menta <gabrielmaxss@outlook.com>
2022-11-10 13:48:06 -0300

    Teste main branch

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -49,6 +49,7 @@ class _LoginScreenState extends State<LoginScreen> {
             RoundedButton(
                 btnText: 'Entrar', onBtnPressed: () => print('log in'))
             //  ajustar com AuthService.logIn
+            //  teste
           ],
         ),
       ),
