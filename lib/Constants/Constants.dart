5af0a37c6427b07a99a3937aa95941eb2f10d5c2
Menta <gabrielmaxss@outlook.com>
2022-11-19 11:39:59 -0300

    upgrade: iniciando melhorias na UI

--- lib/Constants/Constants.dart
+++ lib/Constants/Constants.dart
@@ -2,7 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
+// Cores
 const Color KzipZopColor = Colors.lightGreen;
+
+const defaultFontColor = Colors.black;
+const opositeFontColor = Colors.white;
+
+const Color primaryColorLight = Colors.yellow;
+const Color secondaryColorLight = Colors.yellow;
+const Color tertiaryColorLight = Colors.yellowAccent;
+
+const Color primaryColorDark = Colors.red;
+const Color secondaryColorDark = Colors.red;
+const Color tertiaryColorDark = Colors.red;
+//
 
 final _firestore = FirebaseFirestore.instance;
 final storageRef = FirebaseStorage.instance.ref();
